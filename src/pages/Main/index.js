6742ac02b02608afc81fbfe6676d39c521c8f302
humanless2u <humanless2u@protonmail.com>
2022-05-25 16:35:19 -0300

    main page updated

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,37 @@
-import React from "react";
-import { Title, Container } from "./styles"
+import React, { useState } from 'react';
+import { FaGithub, FaPlus } from 'react-icons/fa'
+import { Title, Container, Form, SubmitButton } from "./styles"
 
 export default function Main() {
+
+    let [newRepo, setNewRepo] = useState('')
+    let handleInput = (e) => setNewRepo(e.target.value)
+
+    let handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('newRepo:', newRepo);
+    }
+
     return (
         <Container>
-            <Title>Main</Title>
+
+            <Title>
+                <FaGithub size={25} />
+                Meus Repositorios
+            </Title>
+
+            <Form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder=" Adicionar Repositorios"
+                    value={newRepo}
+                    onChange={handleInput}
+                />
+                <SubmitButton type='submit'>
+                    <FaPlus color="#FFF" size={14} />
+                </SubmitButton>
+            </Form>
+
         </Container>
     )
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,7 +1,47 @@
 import styled from 'styled-components'
 
-export const Title = styled.h1({
-    color: "#ff0000"
+export const Container = styled.div({
+    maxWidth: 700,
+    backgroundColor: '#fff',
+    borderRadius: 4,
+    boxShadow: '0 0 20px rgba(0, 0, 0, 0.5)',
+    padding: 30,
+    margin: 80,
+    marginLeft: 'auto',
+    marginRight: 'auto',
 })
 
-export const Container = styled.div({})
+export const Title = styled.h1({
+    fontSize: 20,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    svg: {
+        marginRight: 10
+    }
+})
+
+export const Form = styled.form({
+    marginTop: 30,
+    display: 'flex',
+    flexDirection: 'row',
+    input: {
+        flex: 1,
+        border: '1px solid #ddd',
+        padding: '10px 15px',
+        borderRadius: 4,
+        fontSize: 17
+
+    }
+})
+
+export const SubmitButton = styled.button({
+    border: 0,
+    backgroundColor: "#0D2636",
+    borderRadius: 4,
+    marginLeft: 10,
+    padding: "0 15px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+})
